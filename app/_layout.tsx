c2b11cdf33449c16b48fd08372350abdb087970c
safa demirkan <demirkansafa@gmail.com>
2025-06-25 15:08:59 -0700

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false }}>
